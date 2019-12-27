7f2a324d0a61d4f4f62454a7f14e57e57bd79681
LibrarySteve <stephenkylegarrison@gmail.com>
2019-12-27 15:22:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,17 @@ from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
 kbd = Keyboard()
 layout = KeyboardLayoutUS(kbd)
-kbd.send(Keycode.GUI)
-time.sleep(0.2)
-layout.write('notepad')
-time.sleep(1.0)
-kbd.send(Keycode.ENTER)
-time.sleep(0.5)
-for times in range(0,9):
-    layout.write('I will not leave my computer unlocked')
-    time.sleep(0.05)
+kbd.send(Keycode.GUI) ## this is the windows key
+time.sleep(0.80)
+layout.write('notepad') ## this is sparta... i mean notepad
+time.sleep(0.80)
+kbd.send(Keycode.ENTER) ## this is the number of times the loop runs
+time.sleep(0.80)
+for times in range(0,3): 
+    layout.write('I will not leave my computer unlocked') ## This will be typed into notepad
+    time.sleep(0.1)
     kbd.send(Keycode.ENTER)
 
 exit()
-
-
 ## For other keycodes, and documentation on this library check this link:
 ## https://circuitpython.readthedocs.io/projects/hid/en/latest/index.html
